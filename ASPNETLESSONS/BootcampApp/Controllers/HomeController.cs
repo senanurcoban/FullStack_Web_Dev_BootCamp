ca3ed79c97f94a335e967a99840af2e99f37a57c
senanurcoban <senacoban132@gmail.com>
2023-12-12 19:34:03 -0800

    Asp.Net Mvc first project BootcampApp completed

--- ASPNETLESSONS/BootcampApp/Controllers/HomeController.cs
+++ ASPNETLESSONS/BootcampApp/Controllers/HomeController.cs
@@ -7,15 +7,10 @@ namespace BootcampApp.Controllers;
 public class HomeController : Controller
 {
    
-
     public IActionResult Index()
     {
-       var kurs = new Bootcamp();
-        kurs.Id = 1;
-        kurs.Title = "Full Stack Bootcamp";
-        kurs.Image = "1.jpg";
-        kurs.Description = "Html, css, jacaScrip ve asp net dersleri.";
-        return View(kurs);
+        return View(Repository.Bootcamps);
+    
     }
 
 
